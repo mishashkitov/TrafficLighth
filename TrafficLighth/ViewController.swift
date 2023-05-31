2467f68a84d443dde5b8a6ce90719b8439797aea
mishashkitov <shkitovm@gmail.com>
2023-05-31 12:46:28 +0500

    Initial Commit

---
+++ TrafficLighth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLighth
+//
+//  Created by Misha Shkitov on 31.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
